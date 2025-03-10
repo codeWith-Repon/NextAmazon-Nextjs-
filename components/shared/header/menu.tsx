b9822c32bb8dd264f7f977267f1ddf7ff6544036
Md Repon <repon7253@gmail.com>
2025-03-10 16:50:23 +0600

    Refactor header component for improved accessibility and styling

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCartIcon, UserIcon } from 'lucide-react'
+import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,14 +6,15 @@ export default function Menu() {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
-        <Link href='/signin' className='header-button'>
-          <UserIcon className='w-8 h-8' />
-          <span className='font-bold'>Sign in</span>
+        <Link href='/signin' className='flex items-center header-button'>
+         Hello, Sign in 
         </Link>
 
         <Link href='/cart' className='header-button'>
-          <ShoppingCartIcon className='w-8 h-8' />
-          <span className='font-bold'>Cart</span>
+         <div className="flex items-end">
+         <ShoppingCartIcon className='w-8 h-8' />
+         Cart
+         </div>
         </Link>
       </nav>
     </div>
