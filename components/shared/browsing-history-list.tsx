ae2428cc463d8d755f4f0b69f21751556e588047
Md Repon <repon7253@gmail.com>
2025-04-03 22:40:35 +0600

    Add browsing history feature to product and home pages(browsing-history-list>page(home)>page[slug]>add-to-browsing-history]

--- components/shared/browsing-history-list.tsx
+++ components/shared/browsing-history-list.tsx
@@ -1,3 +1,4 @@
+'use client';
 import useBrowsingHistory from '@/hooks/use-browsing-history';
 import React, { useEffect, useState } from 'react';
 import { Separator } from '../ui/separator';
@@ -12,13 +13,15 @@ export default function BrowsingHistoryList({
   const { products } = useBrowsingHistory();
 
   return (
-    products.length > 0 && (
+    products.length !== 0 && (
       <div className='bg-background'>
         <Separator className={cn('mb-4', className)} />
         <ProductList
           title="Releted to items that you've viewed"
           type='related'
         />
+        <Separator className='mb-4' />
+        <ProductList title='Your browsing history' type='history' hideDetails />
       </div>
     )
   );
